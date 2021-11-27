1a0e69ea7a42d8655b7fc176062587a5cf9fc5f2
Darderion <Eobard1SA@gmail.com>
2021-11-27 18:00:18 +0300

    Created a repository

---
+++ getpdf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://se.math.spbu.ru/thesis/texts/Maksimenko_Dmitrij_Sergeevich_Bachelor_Thesis_2021_text.pdf
+mv Maksimenko_Dmitrij_Sergeevich_Bachelor_Thesis_2021_text.pdf pdf_example_cw1.pdf
+wget https://se.math.spbu.ru/thesis/texts/Svirin_Evgenij_Aleksandrovich_Bachelor_Thesis_2021_text.pdf
+mv Svirin_Evgenij_Aleksandrovich_Bachelor_Thesis_2021_text.pdf pdf_example_cw2.pdf
+wget https://se.math.spbu.ru/thesis/texts/Tuchina_Anastasija_Igorevna_Bachelor_Report_2019_text.pdf
+mv Tuchina_Anastasija_Igorevna_Bachelor_Report_2019_text.pdf pdf_example_cw3.pdf
